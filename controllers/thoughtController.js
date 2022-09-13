aa8b5ecdeb0507cdcf42cf226822e2ce73d45de9
brake <riaconoo@icloud.com>
2022-09-13 15:08:36 +1000

    added comments to controller/routes - TODOs

---
+++ controllers/thoughtController.js
@@ -0,0 +1,16 @@
+// GET to get all thoughts
+
+// GET to get a single thought by its _id
+
+// POST to create a new thought
+    // dont forget to push the created thought's _id to the asociated user's thoughts array field
+
+// PUT to update a thought by its _id
+
+// DELETE to remove a thought by its _id
+
+// /api/thoughts/:thoughtId/reactions
+    // POST to create a reaction stored in a single thought's reactions array field
+    // DELETE to pull and remove a reaction by the reaction's reactionId value
+
+
