999b31dd4558e0dc39877ca6c65d2fa9774084ec
brake <riaconoo@icloud.com>
2022-09-13 14:56:16 +1000

    created models

---
+++ models/Thought.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            max_length: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+    }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
