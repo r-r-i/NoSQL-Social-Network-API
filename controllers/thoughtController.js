29caade246a293959b84ddc529c4b8b54f3a98b4
brake <riaconoo@icloud.com>
2022-09-15 13:51:12 +1000

    added routes for USER and THOUGHT models

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,16 +1,75 @@
+const { User, Thought, } = require('../models');
+const userController = require('./userController');
+
+module.exports = {
+
 // GET to get all thoughts
-
+    getThoughts(req, res) {
+        Thought.find()
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    },
 // GET to get a single thought by its _id
-
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+        .select('-__V')
+        .then((thought) => 
+            !thought
+            ? res.status(404).json({ message: 'No thought with this ID'})
+            : res.json(thought));
+    },
 // POST to create a new thought
     // dont forget to push the created thought's _id to the asociated user's thoughts array field
-
+    addThought(req, res) {
+        console.log("You are adding a Thought")
+        console.log(req.body)
+        User.findOneAndUpdate(
+            {_id: req.params.studentId },
+            { $addToSet: {thoughts: req.body } },
+            { runValidators: true, new: true }
+        )
+        .then((user) => 
+            !user
+            ? res
+                .status(404)
+                .json({ message: "No user with that ID "})
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    }, 
 // PUT to update a thought by its _id
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((thought) => 
+            !thought
+                ? res.status(404).json({ message: "No thought with that ID"})
+                : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
 
 // DELETE to remove a thought by its _id
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({_id: req.params.thoughtId })
+        .then((thought) => 
+            !thought
+                ? res.status(404).json({ message: 'No thought with that ID'})
+                : Thought.deleteOne({ _id: req.params.thoughtId })
+        )
+        .then(() => res.json({ message: 'Thought deleted!'}))
+        .catch((err) => res.status(500).json(err));
+    },
 
 // /api/thoughts/:thoughtId/reactions
     // POST to create a reaction stored in a single thought's reactions array field
     // DELETE to pull and remove a reaction by the reaction's reactionId value
+
+
+
+};
 
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,17 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+module.exports = {
+
 // GET all users
+    getUsers(req, res) {
+        User.find()
+            .then(async (users) => {
+                const userObj = {
+                    users,
+                    thoughts: await Thought.find()
+                }
+            })
+    },
 
 // GET a single user by its _id and populated thought and friend data
 
@@ -7,7 +20,31 @@
 // PUT to update a user by its _id
 
 // DELETE to remove user by its _id
+deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+        .then((student) => 
+            !student 
+                ? res.status(404).json({ message: "No user with that ID"})
+                : Thought.findOneAndUpdate(
+                    { reactions: req.params.userId },
+                    { $pull: { reactions: req.params.userId } },
+                    { new: true }
+                )
+        )
+        .then((thought) => 
+            !thought
+                ? res.status(404).json({ message: 'User deleted, but no thoughts found'})
+                : res.json({ message: 'User successfully deleted '})
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            })
+
+}
 
 // /api/users/:userId/friends/:friendId
     // POST to add a new friend to a user's friend list
     // DELETE to remove a friend from a user's friend list
+
+};
